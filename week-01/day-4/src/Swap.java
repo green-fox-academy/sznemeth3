12fb03ebb17b5efc5999842b010a83996213aec7
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-15 15:33:25 +0100

    Completed Exercise 8

---
+++ week-01/day-4/src/Swap.java
@@ -0,0 +1,15 @@
+public class Swap {
+    public static void main(String[] args) {
+            // Swap the values of the variables
+            int a = 123;
+            int b = 526;
+            int c = a;
+            int d = b;
+            b = c;
+            a= d;
+
+            System.out.println(a);
+            System.out.println(b);
+    }
+}
+
