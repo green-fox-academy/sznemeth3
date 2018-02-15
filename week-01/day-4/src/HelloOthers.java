c083820ed83f444d3e19305be22948151d98f1f4
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-15 12:46:39 +0100

    Completed Exercise 1-3

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,8 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, Laci!");
+        System.out.println("Hello, Zita!");
+        System.out.println("Hello, Szabolcs!");
+    }
+}
