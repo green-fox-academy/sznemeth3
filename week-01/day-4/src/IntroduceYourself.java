d12e1cd81664ca3564224bc569a528d79f6e00a8
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-15 14:04:35 +0100

    Completed Exercise 4-6

---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Szilard");
+        System.out.println("25");
+        System.out.println("1.83");
+    }
+}
