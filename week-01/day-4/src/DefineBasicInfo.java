8b330f9f56d1252e9b33ebbff6e91b13020874bc
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-15 15:54:49 +0100

    Completed Exercise 10

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,18 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        String AzEnNevem = "Szilard";
+        int kor = 25;
+        double height = 1.82;
+        boolean marriedStatus = false;
+
+        System.out.println(AzEnNevem);
+        System.out.println(kor);
+        System.out.println(height);
+        System.out.println(marriedStatus);
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+    }
+}
