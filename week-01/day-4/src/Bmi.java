f5e55823c7ded532fb936634ced6ea828b1b56d7
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-15 15:43:38 +0100

    Completed Exercise 9

---
+++ week-01/day-4/src/Bmi.java
@@ -0,0 +1,9 @@
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+        //BMI= (mass / height^2)
+        System.out.println("BMI:" + (massInKg /(heightInM*heightInM)) + "%");
+        // Print the Body mass index (BMI) based on these values
+    }
+}
