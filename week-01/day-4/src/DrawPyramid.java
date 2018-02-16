842f4de81a6329586d6ad098132a971d73ebac4b
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-16 12:58:34 +0100

    Completed Exercise 29

---
+++ week-01/day-4/src/DrawPyramid.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Collections;
+public class DrawPyramid {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // pyramid like this:
+        //
+        //
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //
+        // The pyramid should have as many lines as the number was
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type a number");
+        String a = "*";
+        int userInput1 =scanner.nextInt();
+        for (int i = 1; i < userInput1 + 1; i++ ) {
+            if (i == 1) {
+                System.out.println(String.join("", Collections.nCopies( userInput1 - i, " ")) +"*");
+            }
+            else {
+                System.out.println(String.join("", Collections.nCopies( userInput1 - i, " ")) +
+                        String.join("", Collections.nCopies(i + (i-1), "*")) +
+                        String.join("", Collections.nCopies(userInput1 - i, " ")));
+            }
+
+        }
+
+    }
+}
