b88b84297e74e851d827ade25c622d6205e58626
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-16 18:10:31 +0100

    Completed Exercise 34

--- week-01/day-4/src/ParametricAverage.java
+++ week-01/day-4/src/ParametricAverage.java
@@ -1,23 +1,36 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ParametricAverage {
     public static void main(String[] args) {
-        // Write a program that stores a number, and the user has to figure it out.
-        // The user can input guesses, after each guess the program would tell one
-        // of the following:
+        // Write a program that asks for a number.
+        // It would ask this many times to enter an integer,
+        // if all the integers are entered, it should print the sum and average of these
+        // integers like:
         //
-        // The stored number is higher
-        // The stored number is lower
-        // You found the number: 8
+        // Sum: 22, Average: 4.4
         Scanner scanner = new Scanner(System.in);
         System.out.println("Amount of numbers");
         int userInputAmount = scanner.nextInt();
         int i = 0;
+        List<Integer> myList = new ArrayList<Integer>();
         while (i < userInputAmount) {
+            System.out.println("Put a number");
+            int userInputAmount1 = scanner.nextInt();
             //int userInputAmount<i> = scanner.nextInt();
-        }
-        System.out.println("Guess a number");
 
+            myList.add(userInputAmount1);
+            i++;
+        }
+        double sum = 0;
+        int j;
+        for(j = 0; j < myList.size(); j++) {
+            sum += myList.get(j);
+
+        }
+        System.out.println(sum);
+        System.out.println(sum / userInputAmount);
 
     }
 
