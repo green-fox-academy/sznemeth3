d24e135bed20e38c3bd6a6083a7c93dda2d76d2f
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-21 12:28:37 +0100

    Completed exercise 8

--- week-02/day-3/src/PositionSquare.java
+++ week-02/day-3/src/PositionSquare.java
@@ -5,27 +5,29 @@ import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class GoToCenter {
-    public static void mainDraw(Graphics graphics) {
-        // Create a line drawing function that takes 2 parameters:
-        // The x and y coordinates of the line's starting point
-        // and draws a line from that point to the center of the canvas.
-        // Draw 3 lines with that function. Use loop for that.
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
         int x = 0;
         int y = 0;
         Random r = new Random();
         for (int i = 0; i < 3; i++) {
 
-            draw(x + r.nextInt(361), y + r.nextInt(361), graphics);
+            draw(x + r.nextInt(271), y + r.nextInt(271), graphics);
         }
-    }
 
+
+    }
     public static void draw(int x, int y, Graphics graphics) {
-        graphics.drawLine(x,y,WIDTH/2,WIDTH/2);
+        graphics.drawRect(x, y,50,50);
     }
 
-
-    // Don't touch the code below
+    //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
@@ -37,12 +39,13 @@ public class GoToCenter {
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
     }
-
-    static class ImagePanel extends JPanel {
+    static class ImagePanel extends JPanel{
         @Override
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
             mainDraw(graphics);
+
         }
     }
+
 }
