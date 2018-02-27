d40653aa61a655a3ca60d7877e56634322389a92
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-27 11:05:19 +0100

    Created now folder for week 3, Exercise init.

--- week-02/day-1/src/Unique.java
+++ week-02/day-1/src/Unique.java
@@ -1,43 +1,45 @@
 import java.util.Arrays;
+
 public class Unique {
-    public static void main(String[] args) {
-        //  Create a function that takes a list of numbers as a parameter
-        //  Returns a list of numbers where every number in the list occurs only once
-        //  Example
-        System.out.println(Arrays.toString(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34})));
-        int[] printList = unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34});
-        for (int i = 0; i < printList.length; i++) {
-            System.out.print(printList[i] + " ");
-        }
-        //  should print: `[1, 11, 34, 52, 61]`
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as a parameter
+    //  Returns a list of numbers where every number in the list occurs only once
+    //  Example
+    System.out.println(Arrays.toString(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34})));
+    int[] printList = unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34});
+    for (int i = 0; i < printList.length; i++) {
+      System.out.print(printList[i] + " ");
     }
-    public static int[] unique(int inputList[]) {
-        int[]templist = new int[inputList.length];
-        for (int j = 0; j < inputList.length; j++) {
-            int duplicateCounter = 0;
-            for (int i = 0; i < templist.length; i++) {
-                if (templist[i]==inputList[j]) {
-                    duplicateCounter ++;
-                }
-            }
-            if (duplicateCounter==0) {
-                templist[j] = inputList[j];
-            }
+    //  should print: `[1, 11, 34, 52, 61]`
+  }
+
+  public static int[] unique(int inputList[]) {
+    int[] templist = new int[inputList.length];
+    for (int j = 0; j < inputList.length; j++) {
+      int duplicateCounter = 0;
+      for (int i = 0; i < templist.length; i++) {
+        if (templist[i] == inputList[j]) {
+          duplicateCounter++;
         }
-        int zeroCounter = 0;
-        for (int i = 0; i < templist.length; i++) {
-            if (templist[i]==0) {
-                zeroCounter ++;
-            }
-        }
-        int nextIndex = 0;
-        int[] uniqueList = new int[templist.length-zeroCounter];
-        for (int i = 0; i < templist.length; i++) {
-            if (templist[i]!=0){
-                uniqueList[nextIndex] = templist[i];
-                ++nextIndex;
-            }
-        }
-        return uniqueList;
+      }
+      if (duplicateCounter == 0) {
+        templist[j] = inputList[j];
+      }
     }
+    int zeroCounter = 0;
+    for (int i = 0; i < templist.length; i++) {
+      if (templist[i] == 0) {
+        zeroCounter++;
+      }
+    }
+    int nextIndex = 0;
+    int[] uniqueList = new int[templist.length - zeroCounter];
+    for (int i = 0; i < templist.length; i++) {
+      if (templist[i] != 0) {
+        uniqueList[nextIndex] = templist[i];
+        ++nextIndex;
+      }
+    }
+    return uniqueList;
+  }
 }
---
+++ week-03/src/DivideByZero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int divisor = scanner.nextInt();
+
+    try {
+      int result = 12 /divisor;
+      System.out.println(result);
+    }
+    catch (ArithmeticException e) {
+      System.out.println("fail");
+    }
+
+  }
+}
