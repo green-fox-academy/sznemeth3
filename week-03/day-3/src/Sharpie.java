ba884438cc0bb2c9552ae0f854a902fe05aef4ee
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-28 14:21:28 +0100

    Init: Sharpie

---
+++ week-03/day-3/src/Sharpie.java
@@ -0,0 +1,28 @@
+public class Sharpie {
+  String color;
+  Float width;
+  Float inkAmount = 100.0f;
+
+
+  public Sharpie(String color, Float width) {
+    this.color = color;
+    this.width = width;
+  }
+
+  public Sharpie() {
+    this.color = "no color";
+    this.width = 0f;
+  }
+
+  public void use() {
+    inkAmount--;
+  }
+
+  public static void main(String[] args) {
+    Sharpie bigRed = new Sharpie("red",10f);
+    bigRed.use();
+    System.out.println(bigRed.inkAmount);
+
+  }
+
+}
