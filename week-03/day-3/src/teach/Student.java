f97d99f79e9720ee5d85a6d8d600c02ef931cb5f
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-02-28 14:54:12 +0100

    Init; Teacher

---
+++ week-03/day-3/src/teach/Student.java
@@ -0,0 +1,13 @@
+package teach;
+
+public class Student {
+  String name;
+
+  public void learn() {
+
+  }
+  public void question() {
+   Teacher teacher = new Teacher();
+   teacher.answer();
+  }
+}
---
+++ week-03/day-3/src/teach/Teacher.java
@@ -0,0 +1,13 @@
+package teach;
+
+public class Teacher {
+  String name;
+
+  public void teach() {
+    Student student = new Student();
+    student.learn();
+  }
+  public void answer() {
+
+  }
+}
