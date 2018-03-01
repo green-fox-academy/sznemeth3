e8b96e878b0a7094192fa3835f12084b71b17f1c
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-03-01 10:41:47 +0100

    Feat: Power

---
+++ week-03/day-4/src/Power.java
@@ -0,0 +1,15 @@
+public class Power {
+  public static int PowerN(int n, int a) {
+    if (n < 1) {
+      return 1;
+    } else if (a == 1) {
+      return n;
+    } else {
+      return n * PowerN(n, a - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(PowerN(5,2));
+  }
+}
